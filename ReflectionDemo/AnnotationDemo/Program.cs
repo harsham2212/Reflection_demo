ca8657ee3e1ebe5feeeec43c8ffcf125b9f9b56a
Harsh Singh <kmrharsh50@gmail.com>
2021-12-21 12:24:09 +0530

    [Harsh]Added:Demo Reflection_demo feature implemented

---
+++ ReflectionDemo/AnnotationDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnnotationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Annotations Demo");
+            AnnotationsTest.Display();
+            Console.ReadKey();
+        }
+    }
+}
