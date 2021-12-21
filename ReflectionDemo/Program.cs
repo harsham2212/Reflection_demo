e844ecb6aeeb7c910e11876e4a90016237b6e920
Harsh Singh <kmrharsh50@gmail.com>
2021-12-21 12:22:40 +0530

    [Harsh]Added:Demo Reflection_demo feature implemented

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to reflection Demo");
+            ReflectionOnCustomer.ReflectionTest();
+            Console.ReadKey();
+        }
+    }
+}
